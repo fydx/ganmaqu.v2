cb3992452a1852b82aabcd134019aac99bf6c198
fydx <lbgg918@gmail.com>
2014-01-25 21:42:38 +0800

    增加 ResultActivity

---
+++ ganmaqu.v2/src/main/java/com/xstrikers/ganmaquv2/ui/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.xstrikers.ganmaquv2.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by LB on 14-1-25.
+ */
+public class ResultActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
